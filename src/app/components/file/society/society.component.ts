932eef39e7c917727f548d07cdecfb5e9a4b02b8
rajesh2024-krita <rajesh.kritatechnosolutions@gmail.com>
2025-09-09 16:56:01 +0530

    changed xtimes label

--- src/app/components/file/society/society.component.ts
+++ src/app/components/file/society/society.component.ts
@@ -235,7 +235,7 @@ import { of, Subject } from 'rxjs';
                   </div>
 
                   <div>
-                    <label class="block mb-2 text-xs font-medium text-gray-900 dark:text-white">X Times</label>
+                    <label class="block mb-2 text-xs font-medium text-gray-900 dark:text-white">N Times of Share Amount</label>
                     <input type="number" formControlName="xTimes" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-white text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                   </div>
                 </div>
